42e9901302a877823de58cedb92ae570bd971fd5
GUchechi <godswilluchendu7@gmail.com>
2024-08-05 03:10:52 +0100

    added register and login flow

--- js/script.js
+++ js/script.js
@@ -18,3 +18,20 @@ document.querySelector("#account-btn").onclick = () => {
 document.querySelector("#close-form").onclick = () => {
   accountForm.classList.remove("active");
 };
+
+let registerbtn = document.querySelector(".account-form .register-btn");
+let loginbtn = document.querySelector(".account-form .login-btn");
+
+registerbtn.onclick = () => {
+  registerbtn.classList.add("active");
+  loginbtn.classList.remove("active");
+  document.querySelector(".account-form .login-form").classList.remove("active");
+  document.querySelector(".account-form .register-form").classList.add("active");
+};
+
+loginbtn.onclick = () => {
+    registerbtn.classList.remove("active");
+    loginbtn.classList.add("active");
+    document.querySelector(".account-form .login-form").classList.add("active");
+    document.querySelector(".account-form .register-form").classList.remove("active");
+  };
