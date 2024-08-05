14f8c84f03b81a27ff17a5083e91bd714283a46c
GUchechi <godswilluchendu7@gmail.com>
2024-08-05 02:21:08 +0100

    added form

--- js/script.js
+++ js/script.js
@@ -7,3 +7,14 @@ document.querySelector("#cart-btn").onclick = () => {
 document.querySelector("#close").onclick = () => {
   shoppingcart.classList.remove("active");
 };
+
+// account form
+let accountForm = document.querySelector(".account-form");
+
+document.querySelector("#account-btn").onclick = () => {
+  accountForm.classList.add("active");
+};
+
+document.querySelector("#close-form").onclick = () => {
+  accountForm.classList.remove("active");
+};
